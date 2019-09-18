34421a7d4c1e9c1f15c04e0e2bdc5a5a565ec9ac
Rawand Dargali <rdargali@yahoo.com>
2019-09-18 13:29:11 -0500

    commits

---
+++ 04-week/0-monday/labs/JSexercise1.js
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>JS practice</title>
+</head>
+<body>
+    <script>
+        function lvl1exercise1 () {
+            // Declare a variable without instantiating it and return it!
+            var name = 'Rawand Dargali'; 
+        
+            return(name)
+        }
+        
+
+        function lvl1exercise2 () {
+            // Declare and instantiate a number and return it
+            var myNumber = 50
+            console.log(myNumber)
+        }
+        
+        function lvl1exercise3 () {
+            // Declare and instantiate a floating point number that is not a whole number and return it
+
+            var floatNumber = 2.5
+            console.log(floatNumber)
+            
+        }
+        
+        function lvl1exercise4 () {
+            // Declare and instantiate a string "Hello World!" and return it
+            var myString = "Hello World!"
+            console.log(myString)
+            
+        }
+        
+        function lvl1exercise5 () {
+            // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
+            var myArray = ['Hello World!', 4]
+            console.log(myArray)
+            
+        }
+        
+        function lvl1exercise6 () {
+            // Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
+            var myObject = {
+                key1 : 'Hello World!'
+                key2 : 4
+            }
+            console.log(myObject)
+        }
+        
+        function lvl1exercise7 () {
+            // Declare and instantiate a boolean value 'false' and return it
+            var myBoolean = false
+
+            console.log(myBoolean)
+        }
+        
+        
+        
+    </script>
+    
+</body>
+</html>
---
+++ 04-week/2-wednesday/labs/HighOnCoding.js
@@ -0,0 +1,6 @@
+var container = document.getElementById("container")
+
+var header = document.createElement("header")
+var logo = document.createElement('h1')
+var navigation = document.createElement('nav')
+
